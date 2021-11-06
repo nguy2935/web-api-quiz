8e2b248adbde6b82bac3d319c9160b12655e2d53
unknown <nguy2935@umn.edu>
2021-11-06 05:22:18 -0500

    added timer and high score placeholder

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// timer //
+var timer = duration, minutes, seconds; 
+setInterval(function() {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    display.textContent = minutes + ":" + seconds;
+
+    if (--timer < 0) {
+        timer = duration;
+        }
+    }, 1000);
+}
+
+// timer expired popout message //
+if ( < 0) {
+    
+}
